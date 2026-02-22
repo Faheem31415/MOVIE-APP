4306186c765453c24fcd9321121f5a154f4d17c1
shahh faheem <shahfaheem31415@gmail.com>
2026-02-22 14:28:14 +0530

    updated

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,18 +1,17 @@
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const popularMovies= async ()=>{
+    const response=await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data=await response.json();
+    return data.results;
+    
+}
+
+export const searchMovies= async (query)=>{
+    const response=await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data=await response.json();
+    return data.results;
+}
 
 
-
-export const popularMovies = async () => {
-  const response = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/movies`
-  );
-  const data = await response.json();
-  return data.results;
-};
-
-export const searchMovies = async (query) => {
-  const response = await fetch(
-    `${import.meta.env.VITE_BACKEND_URL}/search?query=${encodeURIComponent(query)}`
-  );
-  const data = await response.json();
-  return data.results;
-};
